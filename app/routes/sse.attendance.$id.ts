03b7f343a1b679a08131fec8e0178c7a0d885417
elianiva <dicha.arkana03@gmail.com>
2023-11-24 10:36:40 +0700

    refactor: adjust attendance refresh interval

--- app/routes/sse.attendance.$id.ts
+++ app/routes/sse.attendance.$id.ts
@@ -4,15 +4,18 @@ import { interval } from "remix-utils/timers";
 import type { Attendance } from "~/schema/attendance";
 import { getAllAttendances } from "~/services/attendance.server";
 
+const REFRESH_INTERVAL = 5000;
+
 export async function loader({ request, params, context }: LoaderFunctionArgs) {
 	return eventStream(request.signal, (send) => {
 		async function run() {
 			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			for await (let _ of interval(1000, { signal: request.signal })) {
+			for await (let _ of interval(REFRESH_INTERVAL, { signal: request.signal })) {
 				const attendances = await getAllAttendances(
 					context.ATTENDANCE_QUEUE as KVNamespace,
 					params.id as string
 				);
+				// prevent duplicates with the same id and fullname
 				const uniqueAttendees = attendances.reduce((acc, curr) => {
 					if (acc.find((a) => a.id === curr.id || a.fullname === curr.fullname)) return acc;
 					return acc.concat(curr);
